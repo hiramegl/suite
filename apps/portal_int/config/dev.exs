02dd43d550b3020560b07b77e8d52f6d2a4ada3a
Hiram Galicia <hiram.galicia@scb.se>
2025-05-04 20:23:12 +0200

    Moving Dockerfile and .dockerignore to portal_int for encapsulation. * NOTE! REMOVE ALL BUILD RESOURCES IN HOST, otherwise docker try to use the files in the host system (MacOS):   * rm -rf apps/portal_int/_build/   * rm -rf apps/portal_int/deps/   * rm -rf apps/portal_int/priv/static/assets * In root directory "suite/":   * Build image (verbose):     * docker build -t portal_int:0.1.0 -f apps/portal_int/Dockerfile --no-cache --progress=plain .   * Build image (not verbose)     * docker build -t portal_int:0.1.0 -f apps/portal_int/Dockerfile .   * docker run --name web_portal_int -p 4000:4000/tcp -t portal_int:0.1.0   * docker stop web_portal_int   * docker start web_portal_int   * docker exec -it web_portal_int sh

--- apps/portal_int/config/dev.exs
+++ apps/portal_int/config/dev.exs
@@ -4,7 +4,7 @@ import Config
 config :portal_int, PortalInt.Repo,
   username: "postgres",
   password: "postgres",
-  hostname: "host.docker.internal",
+  hostname: "host.docker.internal", #"localhost"
   port:     "5500",
   database: "portal_int_dev",
   stacktrace: true,
@@ -20,7 +20,7 @@ config :portal_int, PortalInt.Repo,
 config :portal_int, PortalIntWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {0, 0, 0, 0}, port: 4000],
+  http: [ip: {0, 0, 0, 0}, port: 4000], # [ip: {127, 0, 0, 1}, port: 4000],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
--- apps/portal_int/mix.exs
+++ apps/portal_int/mix.exs
@@ -5,10 +5,10 @@ defmodule PortalInt.MixProject do
     [
       app: :portal_int,
       version: "0.1.0",
-      build_path: "../../_build",
+      build_path: "./_build",
       config_path: "./config/config.exs",
-      deps_path: "../../deps",
-      lockfile: "../../mix.lock",
+      deps_path: "./deps",
+      lockfile: "./mix.lock",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
