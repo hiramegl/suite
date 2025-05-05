21fa236c6953d549802acd4f6620beb8fb50f9f3
Hiram Galicia <hiram.galicia@scb.se>
2025-05-05 19:25:01 +0200

    Fixing ports and documentation in portal_int

--- apps/portal_int/config/config.exs
+++ apps/portal_int/config/config.exs
@@ -1,12 +1,3 @@
-# This file is responsible for configuring your umbrella
-# and **all applications** and their dependencies with the
-# help of the Config module.
-#
-# Note that all applications in your umbrella share the
-# same configuration and dependencies, which is why they
-# all use the same configuration file. If you want different
-# configurations or dependencies per app, it is best to
-# move said applications out of the umbrella.
 # This file is responsible for configuring your application
 # and its dependencies with the aid of the Config module.
 #
@@ -74,11 +65,3 @@ config :phoenix, :json_library, Jason
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
-
-# Sample configuration:
-#
-#     config :logger, :console,
-#       level: :info,
-#       format: "$date $time [$level] $metadata$message\n",
-#       metadata: [:user_id]
-#
