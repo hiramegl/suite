cd37cf58105e5bba566eb5a5fb19e7066eedf8d0
Hiram Galicia <hiram.galicia@scb.se>
2025-05-11 17:50:02 +0200

    Removing aku_ext app

--- apps/aku_ext/lib/aku_ext/mailer.ex
+++
@@ -1,3 +0,0 @@
-defmodule AkuExt.Mailer do
-  use Swoosh.Mailer, otp_app: :aku_ext
-end
--- apps/aku_ext/lib/aku_ext/repo.ex
+++
@@ -1,5 +0,0 @@
-defmodule AkuExt.Repo do
-  use Ecto.Repo,
-    otp_app: :aku_ext,
-    adapter: Ecto.Adapters.Postgres
-end
