336f1295cd48c87a853688a8d2dca242ebb83743
Hiram Galicia <hiram.galicia@scb.se>
2025-05-11 17:50:41 +0200

    removing portal_ext app, will be renamed to survey

--- apps/portal_ext/lib/portal_ext/mailer.ex
+++
@@ -1,3 +0,0 @@
-defmodule PortalExt.Mailer do
-  use Swoosh.Mailer, otp_app: :portal_ext
-end
--- apps/portal_ext/lib/portal_ext/repo.ex
+++
@@ -1,5 +0,0 @@
-defmodule PortalExt.Repo do
-  use Ecto.Repo,
-    otp_app: :portal_ext,
-    adapter: Ecto.Adapters.Postgres
-end
