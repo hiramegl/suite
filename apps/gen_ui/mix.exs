e93f1c32a6159d3e5ed9bcde93b58478cffb107e
Hiram Galicia <hiram.galicia@scb.se>
2025-06-16 22:46:51 +0200

    Fixing directories in mix.exs for GenUi and GenLib

--- apps/gen_ui/mix.exs
+++ apps/gen_ui/mix.exs
@@ -5,10 +5,10 @@ defmodule GenUi.MixProject do
     [
       app: :gen_ui,
       version: "0.1.0",
-      build_path: "../../_build",
-      config_path: "../../config/config.exs",
-      deps_path: "../../deps",
-      lockfile: "../../mix.lock",
+      build_path: "./_build",
+      config_path: "./config/config.exs",
+      deps_path: "./deps",
+      lockfile: "./mix.lock",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps()
