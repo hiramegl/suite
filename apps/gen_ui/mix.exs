24a383b42fb9dbc0b5472f64b8c9f49bfadb8a9a
Hiram Galicia <hiram.galicia@scb.se>
2025-06-17 20:18:39 +0200

    Fixing same Phoenix LiveView dependencies in GenUi and Portal to prevent compilation errors

--- apps/gen_ui/mix.exs
+++ apps/gen_ui/mix.exs
@@ -28,6 +28,11 @@ defmodule GenUi.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       # {:sibling_app_in_umbrella, in_umbrella: true}
+      {:phoenix_html, "~> 4.1"},
+      {:phoenix_live_view,
+       git: "/Users/hiram/Projects/web/suite/custom/phoenix_live_view",
+       branch: "hiram_0_3",
+       override: true}, # override dependency in phoenix_live_dashboard
     ]
   end
 end
